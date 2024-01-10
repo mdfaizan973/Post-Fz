6bd514292b171eef13cf3ff0509ad312b20fdf8a
mdfaizan973 <faizan.md9735@gmail.com>
2024-01-10 21:59:17 +0530

    Able to make video

--- react-post/src/Components/Axios.jsx
+++ react-post/src/Components/Axios.jsx
@@ -1,27 +1,35 @@
-import axios from "axios";
+import { useEffect, useState } from "react";
 import "./Styles/Axios.css";
-import { useState, useEffect } from "react";
+import axios from "axios";
 export default function Axios() {
-  const [datas, setDatas] = useState([]);
+  const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true);
     axios
       .get(
         `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-coffee?limit=5&page=${page}`
       )
-      .then((response) => {
-        console.log("Response:", response.data.data);
-        setDatas(response.data.data);
+      .then((res) => {
+        // console.log(res.data.data);
+        setData(res.data.data);
+        setLoading(false);
       })
-      .catch((error) => {
-        console.error("Error:-", error);
+      .catch((err) => {
+        console.log(err.message);
       });
   }, [page]);
 
-  const handlepage = (val) => {
+  // console.log("data", data);
+
+  const handlepagination = (val) => {
     setPage(page + val);
   };
+
+  if (loading) {
+    return <h1>Loading.......</h1>;
+  }
   return (
     <div id="main">
       <table>
@@ -37,7 +45,9 @@ export default function Axios() {
           </tr>
         </thead>
         <tbody>
-          {datas.map((ele, i) => (
+          {/* Mapping data */}
+
+          {data.map((ele, i) => (
             <tr key={i}>
               <td>{ele.description}</td> <td>{ele.id}</td>
               <td>
@@ -49,12 +59,14 @@ export default function Axios() {
             </tr>
           ))}
         </tbody>
+        {/*Pagintaion Buttons  */}
+
         <div className="buttons">
-          <button disabled={page === 1} onClick={() => handlepage(-1)}>
+          <button disabled={page == 1} onClick={() => handlepagination(-1)}>
             Prev
           </button>
           <button>{page}</button>
-          <button onClick={() => handlepage(+1)}>Next</button>
+          <button onClick={() => handlepagination(1)}>Next</button>
         </div>
       </table>
     </div>
