33dc3af277fb5bbe6fbdd8defbde1eb203f2bce1
mdfaizan973 <faizan.md9735@gmail.com>
2024-01-15 19:57:21 +0530

    done reverseNumber

---
+++ JAVASCRIPT/reverse_number.js
@@ -0,0 +1,15 @@
+function reverseNumber(num) {
+  let reversed = 0;
+
+  while (num !== 0) {
+    reversed = reversed * 10 + (num % 10);
+    num = Math.floor(num / 10);
+  }
+  return reversed;
+}
+
+// Example usage
+let originalNumber = 12345;
+let reversedNumber = reverseNumber(originalNumber);
+console.log(`Original: ${originalNumber}`);
+console.log(`Reversed: ${reversedNumber}`);
