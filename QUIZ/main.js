d0fba3cde41ab721b2546b5a35f87c4bf0b4619a
mdfaizan973 <faizan.md9735@gmail.com>
2024-01-29 10:56:55 +0530

    quiz

---
+++ QUIZ/main.js
@@ -0,0 +1,25 @@
+// 24 jan 2024
+
+function jan24() {
+  let x;
+  console.log(x == undefined);
+
+  const type = null;
+  console.log(typeof type);
+
+  console.log("2" > 1);
+}
+// jan24();
+
+// 29 jan 2024
+function jan29() {
+  console.log(a);
+  let a;
+
+  console.log(b);
+  var b;
+
+  // console.log(c);
+  // const c;
+}
+// jan29();
