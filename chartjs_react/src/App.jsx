504cc0b3f98f55ed9609984dc25c8f5e196f0c8d
mdfaizan973 <faizan.md9735@gmail.com>
2024-03-26 15:52:05 +0530

    added bar chart

---
+++ chartjs_react/src/App.jsx
@@ -0,0 +1,9 @@
+import BarCharts from "./Components/BarChart";
+
+export default function App() {
+  return (
+    <div>
+      <BarCharts />
+    </div>
+  );
+}
---
+++ chartjs_react/src/Components/BarChart.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import "./Styles.css";
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+export default function BarCharts() {
+  // https://dummyjson.com/products
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.products);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(data);
+  return (
+    <div className="main_containter">
+      <h1>Stock Levels by Brand</h1>
+      {/* here i will use Bar chart */}
+      <ResponsiveContainer aspect={5}>
+        <BarChart data={data}>
+          <XAxis dataKey="brand" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="stock" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+// npm install recharts
